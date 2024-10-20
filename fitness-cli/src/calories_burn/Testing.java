05aa6820cef74d2f634aadfb71d837086b099bfb
Yim Ho Hin <hhyim3-c@my.cityu.edu.hk>
2024-10-20 22:34:37 +0800

    modified:   fitness-cli/.project 	modified:   fitness-cli/src/calories_burn/Testing.java

--- fitness-cli/src/calories_burn/Testing.java
+++ fitness-cli/src/calories_burn/Testing.java
@@ -1,7 +1,8 @@
 package calories_burn;
 
 public class Testing {
-	    public static void main(String[] args) {
-	    	System.out.println("Hello World");
-	    }
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hello World2");
+	}
 }
